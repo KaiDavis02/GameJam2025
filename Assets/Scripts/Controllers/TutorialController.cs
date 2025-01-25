e5fb3861c96e48233504157374b8e91e04b96216
Kai Davis <kd2g20@soton.ac.uk>
2025-01-25 16:22:57 +0000

    fixed UI anchors

--- Assets/Scripts/Controllers/TutorialController.cs
+++ Assets/Scripts/Controllers/TutorialController.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class TutorialController : MonoBehaviour
 {
+    [SerializeField] GameObject speechBubblePrefab;
+    [SerializeField] GameObject draggablePrefab;
+    [SerializeField] Canvas canvas;
+
+    Problem p;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameState.enterTutorial();
+        p = GameState.tutorialProblem;
+
+        GameObject testBub = Instantiate(speechBubblePrefab, canvas.transform);
+        GameState.genDebugProblem();
+        p = GameState.debugProblem;
+
+        Debug.Log(p.leftIndex);
+        Debug.Log(p.bubblesLeft[p.leftIndex].text);
+
+        testBub.GetComponent<SpeechBubble>().bubble = p.bubblesLeft[p.leftIndex];
+        testBub.GetComponent<SpeechBubble>().writeText();
     }
 
     // Update is called once per frame
